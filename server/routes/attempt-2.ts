e9e576ddc4080f2b9ae697ff80446407abefcabd
onmax <maximogarciamtnez@gmail.com>
2025-02-13 07:41:45 +0100

    added await mod() to attempt 2

--- server/routes/attempt-2.ts
+++ server/routes/attempt-2.ts
@@ -4,7 +4,10 @@ import init, { PublicKey } from '@nimiq/core/web'
 import mod from './nimiq.wasm'
 
 export default defineEventHandler(async () => {
+  console.log('Running attemp 2')
   await init(mod)
+  // await mod()
+  console.log('______Running attemp 2')
   const publicKey = '82d80b86d9bf1906832e9f0ba4fa69018792f59190075c396b0e85aeac444e55'
   const key = PublicKey.fromHex(publicKey)
   return new Response(key.toHex())
