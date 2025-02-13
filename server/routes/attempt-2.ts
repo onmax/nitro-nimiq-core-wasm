1906cceb3db4406b9274ee829c01d449932866de
onmax <maximogarciamtnez@gmail.com>
2025-02-13 07:43:27 +0100

    clean up

--- server/routes/attempt-2.ts
+++ server/routes/attempt-2.ts
@@ -10,5 +10,5 @@ export default defineEventHandler(async () => {
   console.log('______Running attemp 2')
   const publicKey = '82d80b86d9bf1906832e9f0ba4fa69018792f59190075c396b0e85aeac444e55'
   const key = PublicKey.fromHex(publicKey)
-  return new Response(key.toHex())
+  return key.toHex()
 })
